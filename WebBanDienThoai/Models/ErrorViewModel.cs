2eda89d5370cdeab0a41649400736122695e2ce4
trangthu0401 <ngtttrang040105@gmail.com>
2025-09-21 18:59:25 +0700

    Add project files.

---
+++ WebBanDienThoai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanDienThoai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
