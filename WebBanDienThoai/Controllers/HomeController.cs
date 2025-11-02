1661df90ebca8e6a78f921ed59b8c016b4d67c58
TRUNG BAYBY <20072005trung@gmail.com>
2025-11-02 21:08:20 +0700

    bug

--- WebBanDienThoai/Controllers/HomeController.cs
+++ WebBanDienThoai/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace WebBanDienThoai.Controllers
             return View();
         }
 
+        public IActionResult Home()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
